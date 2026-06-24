8fe724fcadf51b3e3ef62730581d09771dc405c3
abdelkader <abdelkader.lakhdar@epitech.eu>
2026-06-24 15:35:33 +0200

    [FEAT] Effet de hover Ok

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,11 +1,11 @@
 import pygame
-from data.config_manager import LARGEUR,HAUTEUR,BLUE,RED,WHITE,ORANGE,GREEN,COLORS
+from data.config_manager import LARGEUR,HAUTEUR,BLUE,RED,WHITE,ORANGE,GREEN,COLORS,TITLE
 
 
 
 def showMenu(screen) : 
     font = pygame.font.SysFont("book antiqua",48)
-    text = font.render("S0undB0x Project V0.1", 1,WHITE)
+    text = font.render(TITLE, 1,WHITE)
     textpos = text.get_rect()
     textpos = textpos.center
     screen.blit(text, textpos)
@@ -16,5 +16,12 @@ def showMenu(screen) :
     textSoundPos = textSound.get_rect()
     textSoundPos.center = soundBtn.center
     screen.blit(textSound,textSoundPos)
+
+    mouse = pygame.mouse.get_pos()
+
+    if soundBtn.collidepoint(mouse) :
+        playBtn = pygame.draw.rect(screen, GREEN, (LARGEUR/6 ,HAUTEUR/6 ,80,80),border_radius = 35)
+        playBtnBorder = pygame.draw.rect(screen, BLUE,  (LARGEUR/6 ,HAUTEUR/6 ,80,80),border_radius = 35,width = 2)
+        screen.blit(textSound,textSoundPos)
 
     pygame.display.flip()
